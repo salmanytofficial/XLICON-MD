b686bd0d91598c42186d81afb23238cd2a6e7013
S4 Salman YT <140977479+salmanytofficial@users.noreply.github.com>
2023-09-25 16:08:04 +0500

    Fixed-By-@salmanytofficial
    Fixed-By-@salmanytofficial

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 //═══════[Required Variables]════════\\
 global.owner = process.env.OWNER_NUMBER || "923184070915"  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
-global.mongodb = process.env.MONGODB_URI || "mongodb+srv://salman:s4salmanyt@cluster0.vjr9j7i.mongodb.net/?retryWrites=true&w=majority"
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://salman:s4salmanyt@cluster0.frtg3vx.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'xheikhsalman4422@gmail.com'
 global.github = 'https://github.com/salmanytofficial/XLICON-MD'
