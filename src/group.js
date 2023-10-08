09c6fd87ebbc2a8616eb383890e407fd3dce1a4b
ɪɴɴᴏxᴇɴᴛ-ᴢᴜʙɪ <143280418+Maher-Zubair@users.noreply.github.com>
2023-10-08 20:04:11 +0500

    Fixed-

--- src/group.js
+++ src/group.js
@@ -6,7 +6,7 @@
  * @project_name : Xlicon-MD
  * @author : salmanytofficial <https://github.com/salmanytofficial>
  * @description : Xlicon,A Multi-functional whatsapp bot.
- * @version 0.0.6 
+ * @version 0.0.6
  **/
 
 const { sck, sck1,cmd, jsonformat, botpic, TelegraPh, RandomXP, Config, tlang, warndb, sleep,getAdmin,getBuffer, prefix } = require('../lib')
