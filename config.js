d4122a163ac3335d852a60e74cdee7682b215855
Salman YT Official <140977479+salmanytofficial@users.noreply.github.com>
2023-10-08 18:42:17 +0500

    Fixed-By-@salmanytofficial
    Fixed-By-@salmanytofficial

--- config.js
+++ config.js
@@ -16,8 +16,8 @@ global.website = 'https://github.com/salmanytofficial/XLICON-MD' //wa.me/+910000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/3c341828d86ee7a89c73f.jpg'
 module.exports = {
   sessionName:  process.env.SESSION_ID ||  " ",  //PUT Session Id Here, 
-  author:  process.env.PACK_AUTHER ||  'XLICON BOT',
-  packname:  process.env.PACK_NAME || 'MADE BY SALMAN AHMAD',
+  author:  process.env.PACK_AUTHER ||  '',
+  packname:  process.env.PACK_NAME || '',
    
   botname:   process.env.BOT_NAME === undefined ? "Xlicon-Md" : process.env.BOT_NAME,
   ownername: process.env.OWNER_NAME === undefined ? 'salmanytofficial' : process.env.OWNER_NAME,  
