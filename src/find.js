404ddb944f55e33c8e1cb0b5ecb1edfc2b81cffa
Salman YT Official <140977479+salmanytofficial@users.noreply.github.com>
2023-11-10 15:33:39 +0500

    Fixed-By-@salmanytofficial
    Fixed-By-@salmanytofficial

---
+++ src/find.js
@@ -0,0 +1,23 @@
+const { tlang,shazam,prefix,cmd } = require('../lib')
+let yts = require("secktor-pack");
+cmd({
+        pattern: "find",
+        category: "misc",
+        desc: "Finds info about song"
+    },
+    async(Void, citel, args) => {
+        let mime = citel.quoted.mtype
+        if (!citel.quoted) return citel.reply(`Send/Reply Image ${prefix}shaxam`);
+        if (!/audio/.test(mime)) return citel.reply(`Send/Reply audio ${prefix}shazam`);
+        let buff = await citel.quoted.download();
+        let data = await shazam(buff);
+        if (!data.status) return citel.reply(data);
+	let search = await yts(data.title);
+let anu = search.videos[0];
+          let h =  `*Title :* _${data.title}_           
+*Artist :* _${data.artists}_            
+*Album :* _${data.album}_                   
+*Release :* _${data.release_date}
+
+*To Download Song*
+${prefix}ytmp3 ${anu.url}
