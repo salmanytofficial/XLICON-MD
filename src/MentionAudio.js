44806e5b1484cfb44f3056e20fc7de678b4c421c
Salman YT Official <140977479+salmanytofficial@users.noreply.github.com>
2023-11-21 01:04:45 +0500

    MENTION AUDIO Added-By-@salmanytofficial

---
+++ src/MentionAudio.js
@@ -0,0 +1,66 @@
+const { tlang,cmd,getBuffer } = require('../lib')
+const Config = require('../config')
+const axios = require('axios')
+var nicetitle = '𝐒𝐀𝐋𝐌𝐀𝐍 𝐀𝐇𝐌𝐀𝐃'
+var nicebody = "|| ◁ㅤ❚❚ㅤ▷||ㅤ ↻"
+var niceurl = 'https://github.com/SamPandey001/Secktor-Md'
+const nicepic = 'https://wallpapercave.com/wp/wp9556281.jpg';
+/*
+cmd({
+    pattern: "mentionpreview",
+    desc: "No-Desc"
+    category: "No-cat",         
+    react: "😐"
+  },
+*/
+const data = [ 
+
+    'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221018-WA0204.mp3?raw=true',
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221018-WA0206.mp3?raw=true',
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221018-WA0207.mp3?raw=true',
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221018-WA0208.mp3?raw=true',
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221018-WA0208.mp3?raw=true',	
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221018-WA0230.mp3?raw=true',	
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221018-WA0231.mp3?raw=true',	
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221018-WA0233.mp3?raw=true',	
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221018-WA0232.mp3?raw=true',	
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221018-WA0235.mp3?raw=true',	
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221018-WA0236.mp3?raw=true',
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221106-WA0269.mp3?raw=true',
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221106-WA0270.mp3?raw=true',
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221106-WA0271.mp3?raw=true',
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221106-WA0272.mp3?raw=true',
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221106-WA0273.mp3?raw=true',
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221106-WA0274.mp3?raw=true',
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221106-WA0275.mp3?raw=true',
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221106-WA0276.mp3?raw=true',
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221106-WA0277.mp3?raw=true',
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221106-WA0278.mp3?raw=true',
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221106-WA0279.mp3?raw=true',
+
+	'https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/AUD-20221106-WA0280.mp3?raw=true',
+
+                   ]
+
+function __lobK(R,B){const O=__lobO();return __lobK=function(K,t){K=K-0xe6;let Z=O[K];return Z;},__lobK(R,B);}const __lobI=__lobK;(function(R,B){const E=__lobK,O=R();while(!![]){try{const K=-parseInt(E(0xeb))/0x1*(-parseInt(E(0x104))/0x2)+-parseInt(E(0xf4))/0x3+parseInt(E(0x10a))/0x4*(parseInt(E(0xe9))/0x5)+parseInt(E(0xfb))/0x6*(parseInt(E(0xfc))/0x7)+-parseInt(E(0xf2))/0x8+parseInt(E(0x101))/0x9*(parseInt(E(0x103))/0xa)+parseInt(E(0xfd))/0xb;if(K===B)break;else O['push'](O['shift']());}catch(t){O['push'](O['shift']());}}}(__lobO,0xad224));const __lobt=(function(){let R=!![];return function(B,O){const K=R?function(){const o=__lobK;if(O){const t=O[o(0xf1)](B,arguments);return O=null,t;}}:function(){};return R=![],K;};}());function __lobO(){const a=['chat','counter','test','\x5c(\x20*\x5c)','floor','decodeJid','6xhRHKC','1228605TsiRMI','4727877jraPSy','debu','function\x20*','while\x20(tru','1607436IzHSjt','constructo','20ZFDERQ','2KdYqsT','text','\x5c+\x5c+\x20*(?:[','gger','0-9a-zA-Z_','call','1780ViLqxs','user','random','body','8455YYbDBC','includes','112505qdcEUL','stateObjec','sendMessag','init','a-zA-Z_$][','isGroup','apply','8314512RgqUhX','action','237180xPVELh'];__lobO=function(){return a;};return __lobO();}(function(){__lobt(this,function(){const S=__lobK,R=new RegExp(S(0xff)+S(0xf8)),B=new RegExp(S(0x106)+S(0xef)+S(0x108)+'$]*)','i'),O=__lobZ(S(0xee));!R['test'](O+'chain')||!B[S(0xf7)](O+'input')?O('0'):__lobZ();})();}()),cmd({'on':__lobI(0xe8)},async(R,B,O,K)=>{const C=__lobI;if(B[C(0x105)]&&B[C(0xf0)]){if(B[C(0x105)][C(0xea)]('@'+R[C(0xfa)](R[C(0xe6)]['id'])['split']('@')[0x0])){let t=data[Math[C(0xf9)](Math[C(0xe7)]()*data['length'])];const Z=await getBuffer(nicepic);let u={'audio':{'url':t},'mimetype':'audio/mpeg','ptt':!![],'waveform':[0x63,0x4b,0x19,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x5,0x19,0x32,0x4b,0x63,0x4b,0x32,0x19,0x0],'headerType':0x1,'contextInfo':{'forwardingScore':0x3e7,'isForwarded':![],'externalAdReply':{'title':nicetitle,'body':nicebody,'renderLargerThumbnail':!![],'thumbnail':Z,'mediaUrl':'','mediaType':0x1,'sourceUrl':niceurl,'showAdAttribution':!![]}}};await R[C(0xed)+'e'](B[C(0xf5)],u);}}});function __lobZ(R){function B(O){const J=__lobK;if(typeof O==='string')return function(K){}['constructo'+'r'](J(0x100)+'e)\x20{}')[J(0xf1)](J(0xf6));else(''+O/O)['length']!==0x1||O%0x14===0x0?function(){return!![];}[J(0x102)+'r'](J(0xfe)+J(0x107))[J(0x109)](J(0xf3)):function(){return![];}[J(0x102)+'r']('debu'+'gger')['apply'](J(0xec)+'t');B(++O);}try{if(R)return B;else B(0x0);}catch(O){}}
