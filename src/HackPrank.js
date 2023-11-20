85ee722ab2148547761fad6c6be9337c3dec7726
Salman YT Official <140977479+salmanytofficial@users.noreply.github.com>
2023-11-21 01:08:23 +0500

    HACK PRANK Added-By-@salmanytofficial

---
+++ src/HackPrank.js
@@ -0,0 +1,97 @@
+const {fetchJson,cmd, tlang,sleep } = require('../lib')
+
+//---------------------------------------------------------------------------
+
+cmd({
+
+            pattern: "hack",
+
+            category: "prank",
+
+            desc: "hacking prank",
+
+            use: ' ',
+
+            filename: __filename,
+
+        },
+
+        async(Void,citel, text) => {
+
+citel.reply("```Injecting malware```")
+
+await sleep(1000)
+
+citel.reply("```Injecting malware \n 0%```")
+
+await sleep(1000)
+
+citel.reply("```Injecting malware \n █ 10%```")
+
+await sleep(1000)
+
+citel.reply("```Injecting malware \n █ █ 20%```")
+
+await sleep(1000)
+
+citel.reply("```Injecting malware \n █ █ █ 30%```")
+
+await sleep(1000)
+
+citel.reply("```Injecting malware \n █ █ █ █ 40%```")
+
+await sleep(1000)
+
+citel.reply("```Injecting malware \n █ █ █ █ █ 50%```")
+
+await sleep(1000)
+
+citel.reply("```Injecting malware \n █ █ █ █ █ █ 60%```")
+
+await sleep(1000)
+
+citel.reply("```Injecting malware \n █ █ █ █ █ █ █ 70%```")
+
+await sleep(1000)
+
+citel.reply("```Injecting malware \n █ █ █ █ █ █ █ █ 80%```")
+
+await sleep(1000)
+
+citel.reply("```Injecting malware \n █ █ █ █ █ █ █ █ █ 90%```")
+
+await sleep(1000)
+
+citel.reply("```Injecting malware \n █ █ █ █ █ █ █ █ █ █ 100%```")
+
+await sleep(1000)
+
+citel.reply("```System hyjacking on process.. \n Conecting to Server error to find 404 ```")
+
+await sleep(1000)
+
+citel.reply("```Divice successfully connected... \n Riciving data...```")
+
+await sleep(1000)
+
+citel.reply("```Data hyjacked from divice 100% completed \n killing all evidence killing all malwares...```")
+
+await sleep(1000)
+
+citel.reply("``` HACKING COMPLETED ```")
+
+await sleep(1000)
+
+citel.reply("``` SENDING LOG DOCUMENTS...```")
+
+await sleep(1000)
+
+citel.reply("``` SUCCESSFULLY SENT DATA AND Connection disconnected```")
+
+await sleep(1000)
+
+            return citel.reply('*BACKLOGS CLEARED*');
+
+        }
+
+    )
